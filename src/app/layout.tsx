10ada8f9ea9bf8970d1fb49dc255a4a4ccf758e8
Jorge Bierhals Ramos <jorginhodeveloper@gmail.com>
2023-08-23 15:21:37 -0300

    fix: correct file name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
-import { Sidebar } from '@/components/sidebar/sidebar'
+import { Sidebar } from '@/components/sidebar'
 
 import '@/styles/globals.css'
 
