4cf923ab1994d7a02c19aa81690ab5422a38dc03
Jorge Bierhals Ramos <jorginhodeveloper@gmail.com>
2023-08-23 15:22:55 -0300

    fix: correct alignment of logo and search

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -2,12 +2,14 @@ import { Search } from 'lucide-react'
 
 import { Logo } from '@/components/logo'
 
+import { MainNavigation } from './main-navigation'
+
 export const Sidebar = () => {
   return (
     <aside className="space-y-6 border-r border-zinc-200 px-5 py-8">
       <Logo />
 
-      <div className="flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
+      <div className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
         <Search className="h-5 w-5 text-zinc-500" />
 
         <input
@@ -15,6 +17,8 @@ export const Sidebar = () => {
           placeholder="Search"
         />
       </div>
+
+      <MainNavigation />
     </aside>
   )
 }
