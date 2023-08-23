9f96fccdb5cfc58c9e8187005e210460a7bcf59b
Jorge Bierhals Ramos <jorginhodeveloper@gmail.com>
2023-08-23 12:09:41 -0300

    chore: configure ESLint and Prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
 export default function Home() {
-  return <h1>Start Project</h1>
+  return (
+    <div className="h-screen bg-slate-900 p-8 text-slate-100">
+      <h1 className="flex items-center gap-3 text-2xl font-bold before:flex before:h-8 before:w-0.5 before:bg-sky-500 sm:text-5xl lg:text-6xl">
+        Hello Tailwind
+      </h1>
+      <h2>Hello Tailwind</h2>
+      <p>Hello Tailwind</p>
+
+      <button className="mt-4 rounded-md bg-sky-500 px-2 py-2 font-medium enabled:hover:bg-sky-600 disabled:cursor-not-allowed disabled:opacity-60">
+        Sign in
+      </button>
+    </div>
+  )
 }
