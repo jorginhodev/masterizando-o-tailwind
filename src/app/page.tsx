43f94fc517ea4bd22d76158bcb1c569c648c1ba6
Jorge Bierhals Ramos <jorginhodeveloper@gmail.com>
2023-08-24 10:49:36 -0300

    feat: add SettingsTabs on Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import { SettingsTabs } from '@/components/settings-tabs'
+
 export default function Home() {
-  return <h1>Home</h1>
+  return (
+    <>
+      <h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
+
+      <SettingsTabs />
+    </>
+  )
 }
