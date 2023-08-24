19aad8ab234cc67b434935823ff01109150b69dc
Jorge Bierhals Ramos <jorginhodeveloper@gmail.com>
2023-08-24 11:07:08 -0300

    feat: create the structure of the form

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
         <div className="grid-cols-app grid min-h-screen">
           <Sidebar />
 
-          <main className="px-4 pb-12 pt-8">{children}</main>
+          <main className="px-8 pb-12 pt-8">{children}</main>
         </div>
       </body>
     </html>
